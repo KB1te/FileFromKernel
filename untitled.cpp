dec6de3eb95103a9c51de7ededcfe203b42ea370
KB1te <63610437+KB1te@users.noreply.github.com>
2021-01-22 14:33:59 -0300

    Update untitled.cpp

--- untitled.cpp
+++ untitled.cpp
@@ -6,4 +6,5 @@ int main(){
 	HANDLE	hDevice = CreateFileA("\\DosDevices\\kabaite", GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 	const char *buff = "Hello from userland";
 	DeviceIoControl(hDevice, 0x800, &buff, sizeof(buff), NULL, NULL, NULL, NULL);
+	CloseHandle(hDevice);
 }
